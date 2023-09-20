0c57967494fa19b785d5e5f397d401606eed5806
Srikar Chepuri <42759400+srikar777@users.noreply.github.com>
2023-09-20 12:58:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def get_fruityvice_data(this_fruit_choice):
     # write your own comment -what does the next line do? 
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     # write your own comment - what does this do?
-    retrun fruityvice_normalized
+    return fruityvice_normalized
 
 #import requests
 try:
